38877a62a321dd55664021d458fd52721216b1b3
Suhail <134548047+suhailaboobakar@users.noreply.github.com>
2025-05-29 16:33:58 +0530

    prepared for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,42 @@
-import express from 'express'
-import notesRoutes from './routes/notesRoutes.js'
-import { connectDB } from './config/db.js'
-import dotenv from 'dotenv'
-import rateLimiter from './middleware/rateLimiter.js'
-import cors from 'cors'
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
+import path from "path";
 
-dotenv.config()
+dotenv.config();
 
-
-const port = process.env.PORT || 5001
-const app = express()
-
-
+const port = process.env.PORT || 5001;
+const app = express();
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({
-    origin: 'http://localhost:5173', 
+if (process.env.NODE_ENV !== "production") { // Enable CORS only in development mode
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
-))
-app.use(express.json()) // to parse JSON bodies
-app.use(rateLimiter)
+app.use(express.json()); // to parse JSON bodies
+app.use(rateLimiter);
 
+app.use("/api/notes", notesRoutes);
 
-app.use("/api/notes" , notesRoutes)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // Serve static files from the frontend build directory
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
-    app.listen(port , ()=> {
+  app.listen(port, () => {
     console.log(`server is running on port ${port}`);
-})
-})
+  });
+});
 
-
-
-// 
+//
